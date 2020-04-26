c7012c7362bdbe22ec062902ef4a658fe2f96d2a
Magen Brubaker <magenbrubaker@ufl.edu>
2020-04-26 17:02:29 -0400

    Added while loop

--- assignment6/scripts/main.js
+++ assignment6/scripts/main.js
@@ -4,10 +4,19 @@ var members = [
   'Pat Smear',
   'Taylor Hawkins',
   'Rami Jaffee',
-  'Chris Shiflett'];
+  'Chris Shiflett'
+];
 
 var tour = {
   Chicago: 'July 29 & 30, 2018',
   Seattle: 'September 1, 2018',
   Edmonton: 'September 4, 2018'
 };
+
+var i = 0;
+
+while (i < 5) {
+  console.log('Times running through the loop: ' + i);
+
+  i++;
+}
