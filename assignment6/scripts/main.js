45f300b5902e74a4854c7bbee43bc231cd2f7f3d
Magen Brubaker <magenbrubaker@ufl.edu>
2020-04-26 16:29:40 -0400

    Added tour object to main.js

--- assignment6/scripts/main.js
+++ assignment6/scripts/main.js
@@ -1,1 +1,13 @@
-var members = ['Dave Grohl', 'Nate Mendel', 'Pat Smear', 'Taylor Hawkins', 'Rami Jaffee', 'Chris Shiflett'];
+var members = [
+  'Dave Grohl',
+  'Nate Mendel',
+  'Pat Smear',
+  'Taylor Hawkins',
+  'Rami Jaffee',
+  'Chris Shiflett'];
+
+var tour = {
+  Chicago: 'July 29 & 30, 2018',
+  Seattle: 'September 1, 2018',
+  Edmonton: 'September 4, 2018'
+};
